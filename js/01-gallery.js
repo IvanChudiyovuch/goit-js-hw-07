fe2e58070aee4ba8fe78a5fd9bada31a8faac971
IvanChudiyovuch <ivanchudiyovuch@ukr.net>
2022-05-31 13:30:47 +0300

    add task-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const cardMarkUp = createGalleryCardMarkUp(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', cardMarkUp);
+galleryContainer.addEventListener('click', onGalleryContainerClick);
+
+function onGalleryContainerClick(event) {
+  event.preventDefault();
+
+  console.log('click');
+}
+
+function createGalleryCardMarkUp(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+    `;
+    })
+    .join('');
+}
+
+// console.log(createGalleryCardMarkUp(galleryItems));
+
+// console.log(galleryItems);
