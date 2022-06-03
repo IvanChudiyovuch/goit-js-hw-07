0be28daba01ade1c85ed7e813ef5abc649eda293
IvanChudiyovuch <ivanchudiyovuch@ukr.net>
2022-06-03 22:25:29 +0300

    add final homework 7

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkUp = createGalleryCardMarkUp(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkUp);
+
+const liteBox = new SimpleLightbox('.gallery a', {
+  sourceAttr: 'href',
+  captionDelay: 250,
+  captionPosition: 'bottom',
+});
+
+function createGalleryCardMarkUp(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" title = "${description}"/>
+    </a>`;
+    })
+    .join('');
+}
